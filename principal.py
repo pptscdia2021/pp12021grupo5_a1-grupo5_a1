47ee098125ad892fb17018eb2f2c5001cc3266b2
cokebor <jbordarampe@gmail.com>
2021-11-04 20:15:46 -0300

    Commit tabla

--- principal.py
+++ principal.py
@@ -1,29 +1,38 @@
 
 from os import sep
-from libreria import obtenerCSV,obtenerCSV2,obtenerLista
+from libreria import mostrarTabla, obtenerCSV #,obtenerCSV2,obtenerLista
 
 import pandas as pd
 
 
 
-obtenerCSV('https://www.bolsamadrid.es/esp/aspx/Mercados/Precios.aspx?indice=ESI100000000','ctl00_Contenido_tblAcciones','archivo','id')
+#obtenerCSV('https://www.bolsamadrid.es/esp/aspx/Mercados/Precios.aspx?indice=ESI100000000','ctl00_Contenido_tblAcciones','archivo','id')
 
-#obtenerCSV('https://es.finance.yahoo.com/world-indices/','W(100%)','archivo2','class')
-
-
-lista, lista2=obtenerLista('https://www.bolsamadrid.es/esp/aspx/Mercados/Precios.aspx?indice=ESI100000000','ctl00_Contenido_tblAcciones','archivo','id','Máx.','Nombre')
-
-lista, lista2=obtenerLista('https://www.bolsamadrid.es/esp/aspx/Mercados/Precios.aspx?indice=ESI100000000','ctl00_Contenido_tblAcciones','archivo','id','% Dif.','Nombre')
+obtenerCSV('https://es.finance.yahoo.com/world-indices/','W(100%)','archivo2','class')
 
 
-indiceMax1=lista2.index(max(lista2))
-indiceMax2=lista2.index(min(lista2))
+mostrarTabla('archivo.csv')
 
-print("Nombre: ",lista[indiceMax1] ," Max: ", lista2[indiceMax1])
-print("Nombre: ",lista[indiceMax2] ," Min: ", lista2[indiceMax2])
+mostrarTabla('archivo2.csv')
 
-df = pd.read_csv('archivo.csv')
-print(df)
+
+
+
+
+
+#lista, lista2=obtenerLista('https://www.bolsamadrid.es/esp/aspx/Mercados/Precios.aspx?indice=ESI100000000','ctl00_Contenido_tblAcciones','archivo','id','Máx.','Nombre')
+
+#lista, lista2=obtenerLista('https://www.bolsamadrid.es/esp/aspx/Mercados/Precios.aspx?indice=ESI100000000','ctl00_Contenido_tblAcciones','archivo','id','% Dif.','Nombre')
+
+
+#indiceMax1=lista2.index(max(lista2))
+#indiceMax2=lista2.index(min(lista2))
+
+#print("Nombre: ",lista[indiceMax1] ," Max: ", lista2[indiceMax1])
+#print("Nombre: ",lista[indiceMax2] ," Min: ", lista2[indiceMax2])
+
+#df = pd.read_csv('archivo.csv')
+#print(df)
 
 
 
