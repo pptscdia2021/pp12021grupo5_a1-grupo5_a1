4ac79f6718e26a77ccc364b904add52a2268a5c1
Jorge Bordarampé <jbordarampe@gmail.com>
2021-11-04 22:59:57 -0300

    Add files via upload

--- principal.py
+++ principal.py
@@ -1,6 +1,8 @@
 
 from os import sep
-from libreria import mostrarTabla, obtenerCSV
+from libreria import  obtenerCSV
+
+import pandas as pd
 
 
 print("OBJETIVO 1")
@@ -10,14 +12,39 @@ obtenerCSV('https://www.bolsamadrid.es/esp/aspx/Mercados/Precios.aspx?indice=ESI
 
 obtenerCSV('https://es.finance.yahoo.com/world-indices/','W(100%)','archivo2','class')
 
-
+print()
+print()
+print()
+print()    
 print('ACCIONES DE LA BOLSA DE MADRID')
 print('------------------------------------------------------------------------------------------------')
-mostrarTabla('archivo.csv')
-
+datos=pd.read_csv('archivo.csv', header=0, encoding = "ISO-8859-1")
+print(datos)
+print()
+print()
+print()
+print()
 print('ACCIONES DE LA BOLSA DE finance yahoo')
 print('------------------------------------------------------------------------------------------------')
-mostrarTabla('archivo2.csv')
+datos2=pd.read_csv('archivo2.csv', header=0, encoding = "ISO-8859-1")
+print(datos2)
+print()
+print()
+print()
+print()
+print("OBJETIVO 2")
+print("*************************************************************")
+
+datos["% Dif."]=pd.to_numeric(datos["% Dif."].str.replace(',', '.'))
+
+dfordenado=datos.sort_values(by="% Dif.", ascending=False)
+
+print("Las 2 cotizaciones de mayor ganancia son: ")
+print(dfordenado.head(2)["Nombre"],dfordenado.head(2)["% Dif."])
+print()
+print("Las 2 cotizaciones de menor ganancia son: ")
+print(dfordenado.tail(2)["Nombre"],dfordenado.tail(2)["% Dif."])
+
 
 
 
